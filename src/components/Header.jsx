b0ae3358e9404541c80a600152824e2e033bf95e
kylaknauber <kylaknauber@gmail.com>
2025-09-06 22:18:25 -0400

    modified .gitignore and changed main.jsx to index.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+﻿import "../styles/Header.css"
+import devLogo from "../images/dev-logo.svg"
+export default function Header() {
+    return (
+        <header>
+            <div className="logo-container">
+                <h1>KK</h1>
+                <img src={devLogo}></img>
+            </div>
+            <div className="links-container">
+                <h4 className="link-text">About</h4>
+                <h4 className="link-text">Projects</h4>
+                <h4 className="link-text">Contact</h4>
+                <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/kylaknauber/"
+                    className="link-text">
+                    LinkedIn
+                </a>
+                <a  
+                    target="_blank"
+                    href="https://github.com/kylaknauber"
+                    className="link-text">
+                    GitHub
+                </a>
+            </div>
+        </header>
+    )
+}
