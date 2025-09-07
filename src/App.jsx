1661615a447cd288f17fa9a7e0e2722675dc58c7
kylaknauber <kylaknauber@gmail.com>
2025-09-06 23:46:27 -0400

    Added introduction component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Introduction from './components/Introduction'
 function App() {
 
   return (
-   
-      <Header />
-    
+      <>
+          <Header />
+          <Introduction />
+      </>
   )
 }
 
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,20 @@
+﻿import HeadShot from "../images/KylaK-Pic.JPG"
+import "../styles/Introduction.css"
+export default function Introduction() {
+    return (
+        <div className="intro-container">
+            <div className="intro-text">
+                <h1 className="greeting">Hi, I'm Kyla Knauber!</h1>
+                <h3 className="title">Aspiring Software Developer</h3>
+                <p className="message">
+                    I'm a recent college graduate with a passion for softare development and a knack for problem-solving.
+                    I love creating efficient and user-friendly applications that make a difference!
+                </p>
+                <button className="resume-button">Resume</button>
+            </div>
+            <div className="intro-image">
+                <img src={HeadShot} className="headshot" />
+            </div>
+        </div>
+    )
+}
