b8b63aad3377cf70cfb1882c1107f5eea939633a
kylaknauber <kylaknauber@gmail.com>
2025-09-26 16:10:54 -0400

    Changed the images array into an array of objects to add a caption attribute

--- src/data/data.js
+++ src/data/data.js
@@ -39,7 +39,31 @@ export const programs = [
         description: "Using my knowledge of React and JavaScript, I created a user friendly application which allows users to search for any movie, TV show, and person which is present in TMDB database. I used React Router for a robust navigation system, pull data and queries from TMDB web API, and implemented a log in system using Firebase Authentication. Users can favorite any item they choose which is securely stored in a Firestore Database. Pagination allows for efficient data retrieval to prevent slow rendering.",
         languages: ["React", "JavaScript", "React Router", "Firebase", "Web API", "CSS"],
         link: "https://github.com/kylaknauber/Binge-Index",
-        images: [bingeIndexLogo, bingeIndex1, bingeIndex2, bingeIndex3, bingeIndex4, bingeIndex5],
+        images: [
+            {
+                src: bingeIndexLogo,
+                caption: "Binge Index Logo"
+            }, {
+                src: bingeIndex1,
+                caption: "Home Page"
+            },
+            {
+                src: bingeIndex2,
+                caption: "Media Details Page (showing a TV Show)"
+            },
+            {
+                src: bingeIndex3,
+                caption: "Viewing more details for a TV Show"
+            },
+            {
+                src: bingeIndex4,
+                caption: "Favorites Page (if user is logged in)"
+            },
+            {
+                src: bingeIndex5,
+                caption: "Profile Page for logged in user"
+            }
+        ],
         toggleView: false
     },
     {
@@ -48,7 +72,31 @@ export const programs = [
         description: "Using skills in React, JavaScript, and CSS to create an UNO game for a user to play against the computer. Each player is dealt 7 cards randomly, then the application randomly selects which player to begin the game, to which they place one of their dealt cards. The game continues until one user is left with no cards Single page react application focusing on conditional rendering to create a user-friendly and efficient game. Includes a simple Firebase connection to store user's name, score, and date if they wish to upload such data.",
         languages: ["React", "JavaScript", "Firebase", "CSS"],
         link: "https://github.com/kylaknauber/UNO-Game",
-        images: [unoLogo, uno1, uno2, uno3, uno4, uno5],
+        images: [
+            {
+                src: unoLogo,
+                caption: "Uno Logo"
+            }, {
+                src: uno1,
+                caption: "Beginning game board (white outline on deck signifies which turn)"
+            },
+            {
+                src: uno2,
+                caption: "After player places a card, now the opponent's turn"
+            },
+            {
+                src: uno3,
+                caption: "Player places a Wild Card"
+            },
+            {
+                src: uno4,
+                caption: "Game Over: Player Wins"
+            },
+            {
+                src: uno5,
+                caption: "Player is submitting score to Firestore"
+            }
+        ],
         toggleView: false
     },
     {
@@ -57,7 +105,31 @@ export const programs = [
         description: "Senior Capstone Project | Collaborated with a team to create a cross-platform application to assist patients and physicians in tracking facial paralysis recovery and treatment. Allows patients and physicians to communicate with each other asynchronously via a direct messaging system in the application. Used AI and ML technologies for the device camera to track lighting, faces, etc. to ensure quality images are uploaded and sent to physicians. Used Firebase to store patient and physician data along with patient images. React Native is the backbone of the front-end which allows for a smooth and efficient UI for web and mobile devices.",
         languages: ["React Native", "Firebase", "Node.js", "Google Cloud API", "TensorFlow", "BlazeFace"],
         link: "https://github.com/kylaknauber/Spring2025-GradingFacialParalysis",
-        images: [faceLogo, capstone1, capstone2, capstone3, capstone4, capstone5],
+        images: [
+            {
+                src: faceLogo,
+                caption: "Facial Recognition Logo"
+            }, {
+                src: capstone1,
+                caption: "Physician Home Screen (Admin Physician)"
+            },
+            {
+                src: capstone2,
+                caption: "Patient Creation Screen (Accessed by Admin Physicians)"
+            },
+            {
+                src: capstone3,
+                caption: "Patient Upload Screen (uploading facial images)"
+            },
+            {
+                src: capstone4,
+                caption: "Direct Message Screen between Patient (sender) and Physician (receiver)"
+            },
+            {
+                src: capstone5,
+                caption: "Camera Screen for Patients"
+            }
+        ],
         toggleView: false
     },
     {
@@ -66,7 +138,23 @@ export const programs = [
         description: "Created a Mini C Compiler throughout Compilers class, including various sub projects like Tokenizer, Lexical Analyzer, Syntax Analyzer, Semantic Analyzer. Each project was created with Java, along with JFlex for the Lexical Analyzer, and BYACC/J for the bottom-up parser in the Semantic Analyzer. The Mini C Compiler tests Mini C code files for lexical errors, syntax errors, and semantic errors, in which a successful file would \"execute\" the said file.",
         languages: ["Java", "BYaccJ", "JFlex"],
         link: "https://github.com/kylaknauber/Mini-C-Compiler",
-        images: [compilerLogo, compiler1, compiler2, compiler3],
+        images: [
+            {
+                src: compilerLogo,
+                caption: "Compiler Logo"
+            }, {
+                src: compiler1,
+                caption: "Program is successfully compiled, no lexical, syntax or semantic errors. Shows the indented and formatted code that was read through a text file."
+            },
+            {
+                src: compiler2,
+                caption: "Part 2 of the first image: This is the running environment for said code, along with the returned values."
+            },
+            {
+                src: compiler3,
+                caption: "Program is successfully compiled, showing the indented and formatted code, as well as the running environment and returned values from said code."
+            }
+        ],
         toggleView: false
     },
     {
@@ -75,7 +163,23 @@ export const programs = [
         description: "Recreated Flappy Bird which uses the same format as the original game. In order to play the game, the user would left click or press the space bar to start, and then navigate the bird through the pipes without hitting the ground or pipes. Users can submit their score to Firestore if they choose.",
         languages: ["Vue.js", "JavaScript", "HTML", "CSS", "Firebase"],
         link: "https://github.com/kylaknauber/Flappy-Bird",
-        images: [flappyBirdBird, flappyBird1, flappyBird2, flappyBird3],
+        images: [
+            {
+                src: flappyBirdBird,
+                caption: "Flappy Bird Logo"
+            }, {
+                src: flappyBird1,
+                caption: "Start Screen"
+            },
+            {
+                src: flappyBird2,
+                caption: "Playing the Game"
+            },
+            {
+                src: flappyBird3,
+                caption: "Game Over Screen"
+            }
+        ],
         toggleView: false
     },
     {
@@ -84,7 +188,35 @@ export const programs = [
         description: "Created a application which allows users to pick between playing two games: Towers of Hanoi or a Sliding Puzzle game. This was my first Angular application created in my web programming class.",
         languages: ["Angular", "TypeScript", "HTML", "CSS"],
         link: "https://github.com/kylaknauber/TOH-Sliding-Puzzle",
-        images: [toh, slidingPuzzle, angular1, angular2, angular3, angular4, angular5],
+        images: [
+            {
+                src: toh,
+                caption: "TOH Logo"
+            }, {
+                src: slidingPuzzle,
+                caption: "Sliding Puzzle Logo"
+            },
+            {
+                src: angular1,
+                caption: "Beginning the TOH Game"
+            },
+            {
+                src: angular2,
+                caption: "User is moving the blocks between the 3 pegs"
+            },
+            {
+                src: angular3,
+                caption: "TOH Complete"
+            },
+            {
+                src: angular4,
+                caption: "Beginning the Sliding Puzzle Game"
+            },
+            {
+                src: angular5,
+                caption: "User is moving the block (3) to the open slot"
+            }
+        ],
         toggleView: false
     }
 
