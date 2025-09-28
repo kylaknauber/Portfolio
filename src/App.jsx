2acb6c6723cfead0d78cf2507f4427e3944cc338
kylaknauber <kylaknauber@gmail.com>
2025-09-27 20:40:40 -0400

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Introduction from './components/Introduction'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
           <About />
           <Projects />
           <Contact />
+          <Footer />
       </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,52 @@
+﻿import "../styles/footer.css"
+import devLogo from "../images/dev-logo.svg"
+
+export default function Footer() {
+    const pictureCredits = [
+        {
+            name: "Binge Index Icon",
+            link: "https://www.flaticon.com/free-icons/cinema",
+            source: "Cinema icons created by xnimrodx - Flaticon"
+        },
+        {
+            name: "Uno Logo",
+            link: "https://commons.wikimedia.org/wiki/File:UNO_Logo.svg",
+            source: "Public domain, via Wikimedia Commons"
+        },
+        {
+            name: "Facial Recognition Logo",
+            link: "https://www.flaticon.com/free-icons/face-recognition",
+            source: "Face recognition icons created by Andrean Prabowo - Flaticon"
+        },
+        {
+            name: "Compiler Logo",
+            link: "https://www.flaticon.com/free-icons/compiler",
+            source: "Compiler icons created by Karyative - Flaticon"
+        },
+        {
+            name: "Flappy Bird Logo",
+            link: "https://pnghut.com",
+            source: null
+        },
+    ]
+
+    return (
+        <footer className="footer-container">
+            <div className="logo">
+                <h1>KK</h1>
+                <img src={devLogo}></img>
+            </div>
+            <div className="my-info">
+                <p>Portfolio created by Kyla Knauber</p>
+            </div>
+            <div className="sources">
+                <p>Sources</p>
+                <div>
+                    {pictureCredits.map(picture => (
+                        <a href={picture.link} title={picture.source}>{picture.name}</a>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    )
+}
